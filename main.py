b2049d2f947e209b8b3dbda27938ac6e6497e026
mkbertrand <mileskbertrand@gmail.com>
2026-07-07 19:17:37 -0500

    Followup

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ class ReviveView(discord.ui.View):
 
     @discord.ui.button(label='Yes', style=discord.ButtonStyle.blurple)
     async def revive_cabin(self, interaction: discord.Interaction, button: discord.ui.Button):
-
+        await interaction.response.defer(ephemeral=BOT_COMMAND_EPHEMERALITY)
         member = interaction.guild.get_member(self.cabin.camper_id)
         await bot.get_channel(self.cabin.channel_id).edit(category=cabins_active_category, overwrites={
             interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
@@ -152,7 +152,7 @@ class ReviveView(discord.ui.View):
 
         await set_roles(member, True)
 
-        await interaction.response.send_message('Good to go!', ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send('Good to go!', ephemeral=BOT_COMMAND_EPHEMERALITY)
         self.stop()
 
     @discord.ui.button(label='No', style=discord.ButtonStyle.gray)
@@ -164,13 +164,15 @@ class ReviveView(discord.ui.View):
 @app_commands.checks.has_permissions(moderate_members=True)
 @bot.tree.command(name='cabin', description='Make a personal cabin for a camper.', guild=GUILD)
 async def find_cabin(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.defer(ephemeral=BOT_COMMAND_EPHEMERALITY)
+
     cabin = get_cabin_by_camper(member.id)
 
     if cabin and cabin.in_use:
-        await interaction.response.send_message(f'This camper already has a cabin: <#{cabin.channel_id}>', ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send(f'This camper already has a cabin: <#{cabin.channel_id}>', ephemeral=BOT_COMMAND_EPHEMERALITY)
         return
     elif cabin:
-        await interaction.response.send_message('It looks like Mosin just finished cleaning this cabin. Should I give it back to the camper?', view=ReviveView(cabin), ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send('It looks like Mosin just finished cleaning this cabin. Should I give it back to the camper?', view=ReviveView(cabin), ephemeral=BOT_COMMAND_EPHEMERALITY)
         return
 
     await set_roles(member, True)
@@ -188,19 +190,20 @@ async def find_cabin(interaction: discord.Interaction, member: discord.Member):
         })
     append_cabin(Cabin(member.id, cabin_channel.id, cabin_number_current(), True))
     cabin_number_current_increment()
-    await interaction.response.send_message(f'Made a cabin: <#{cabin_channel.id}>', ephemeral=BOT_COMMAND_EPHEMERALITY)
+    await interaction.followup.send(f'Made a cabin: <#{cabin_channel.id}>', ephemeral=BOT_COMMAND_EPHEMERALITY)
 
 @app_commands.default_permissions(moderate_members=True)
 @app_commands.checks.has_permissions(moderate_members=True)
 @bot.tree.command(name='dcabin', description='Decomission a camper\'s cabin.', guild=GUILD)
 async def decomission_cabin(interaction: discord.Interaction, cabin_no: int):
+    await interaction.response.defer(ephemeral=BOT_COMMAND_EPHEMERALITY)
 
     cabin = get_cabin_by_number(cabin_no)
     if not cabin:
-        await interaction.response.send_message(f'I couldn\'t find that cabin!', ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send(f'I couldn\'t find that cabin!', ephemeral=BOT_COMMAND_EPHEMERALITY)
         return
     elif not cabin.in_use:
-        await interaction.response.send_message(f'<#{cabin.channel_id}> is out of comission!', ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send(f'<#{cabin.channel_id}> is out of comission!', ephemeral=BOT_COMMAND_EPHEMERALITY)
         return
     
     global cabins_decomissioned_category
@@ -216,18 +219,20 @@ async def decomission_cabin(interaction: discord.Interaction, cabin_no: int):
         member: discord.PermissionOverwrite(read_messages=False)
     })
     cabin_set_in_use(cabin, False)
-    await interaction.response.send_message(f'<#{cabin.channel_id}> is out of comission!', ephemeral=BOT_COMMAND_EPHEMERALITY)
+    await interaction.followup.send(f'<#{cabin.channel_id}> is out of comission!', ephemeral=BOT_COMMAND_EPHEMERALITY)
 
 @app_commands.default_permissions(moderate_members=True)
 @app_commands.checks.has_permissions(moderate_members=True)
 @bot.tree.command(name='ecabin', description='Explode a camper\'s cabin.', guild=GUILD)
 async def explode_cabin_command(interaction: discord.Interaction, cabin_no: int):
+    await interaction.response.defer(ephemeral=BOT_COMMAND_EPHEMERALITY)
+
     cabin = get_cabin_by_number(cabin_no)
     if not cabin:
-        await interaction.response.send_message(f'I couldn\'t find that cabin!', ephemeral=BOT_COMMAND_EPHEMERALITY)
+        await interaction.followup.send(f'I couldn\'t find that cabin!', ephemeral=BOT_COMMAND_EPHEMERALITY)
         return
 
     await explode_cabin(interaction.guild, cabin)
-    await interaction.response.send_message(f'It has been done.', ephemeral=BOT_COMMAND_EPHEMERALITY)
+    await interaction.followup.send(f'It has been done.', ephemeral=BOT_COMMAND_EPHEMERALITY)
 
 bot.run(DISCORD_API_KEY)
