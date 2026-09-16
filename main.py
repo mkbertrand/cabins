604e54a29e08f79d7cc96649832e59a3f0787f48
mkbertrand <mileskbertrand@gmail.com>
2026-09-16 13:54:45 -0500

    Added guild_names and channel_names

--- main.py
+++ main.py
@@ -250,9 +250,9 @@ async def log_cabin(interaction: discord.Interaction, cabin_no: int):
     channel = await interaction.guild.fetch_channel(cabin.channel_id)
     messages = []
     async for message in channel.history(limit=None):
-        messages.append({'message_id': message.id, 'channel_id': channel.id, 'author_id': message.author.id, 'author_name': message.author.name, 'content': message.content, 'guild_id': guild.id})
+        messages.append({'message_id': message.id, 'channel_id': channel.id, 'author_id': message.author.id, 'author_name': message.author.name, 'content': message.content, 'guild_id': interaction.guild.id})
 
-    pdf = make_pdf(message_log(messages))
+    pdf = make_pdf(message_log(messages, {interaction.guild.id: interaction.guild.name}, {channel.id: channel.name}))
     await interaction.followup.send(
         file=discord.File(io.BytesIO(pdf), filename=f'logs_cabin_{cabin_no}.pdf'),
         ephemeral=True,
